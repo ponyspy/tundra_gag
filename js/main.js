344fba16dd6eaa090d7fdb767324fe62e816d8e4
ponyspy <desade4me@gmail.com>
2019-03-12 17:09:14 +0300

    styl

--- js/main.js
+++ js/main.js
@@ -14,13 +14,15 @@ $(function() {
 		gyroscope: true
 	});
 
-	$('.logo').on('click', function(e) {
+	$(document).on('click', '.logo.active', function(e) {
+		$('.logo').removeClass('active');
 		$('.bg_block').removeClass('blur');
 		$('.content_block').removeClass('show');
 		$('.footer_block').removeClass('scroll');
 	});
 
 	$('.bg_block').on('click', function(e) {
+		$('.logo').addClass('active');
 		$('.bg_block').addClass('blur');
 		$('.content_block').addClass('show');
 		$('.footer_block').addClass('scroll');
